1864220cc56568eb84deb374380a78cfd5483412
Eugenia Kolodeichuk <80124589+EugeniaKolodeichuk@users.noreply.github.com>
2025-07-17 19:12:49 +0300

    + app header

--- movie-react-app/src/App.jsx
+++ movie-react-app/src/App.jsx
@@ -1,33 +1,21 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-
-const Card = ({ title }) => {
-  const [count, setCount] = useState(0);
-  const [hasLiked, setHasLiked] = useState(false);
-
-  useEffect(() => {
-    console.log(`Card with title "${title}" has been liked: ${hasLiked}`);
-  }, [hasLiked]);
-
-  return (
-    <div className='card' onClick={() => setCount(count + 1)}>
-      <h2>{title} <br /> {count || null}</h2>
-
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? 'Liked' : 'Like'}
-      </button>
-    </div>
-  )
-}
+import React, { useState } from 'react'
+import Search from './components/Search'
 
 const App = () => {
-  return (
-    <div className='card-container'>
-      <Card title="Star War" rating={5} isCool={true} />
-      <Card title="Avatar" />
-      <Card title="The Lion King" />
+  const [searchTerm, setSearchTerm] = useState('');
 
-    </div>
+  return (
+    <main>
+      <div className='pattern' />
+      <div className='wrapper'>
+        <header>
+          <img src='./hero.png' alt='Hero Banner' />
+          <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+    </main>
   )
 }
 
